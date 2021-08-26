ddb4f710f65eeccb215ad9d70f9a140f5af7d5e6
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-08-26 15:38:46 +0900

    custom decorator / interceptor 연습

---
+++ src/common/interceptors/undefinedToNull.interceptors.ts
@@ -0,0 +1,18 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class UndefinedToNullInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log('Before...');
+
+    return next
+      .handle()
+      .pipe(map((data) => (data === undefined ? null : data)));
+  }
+}
