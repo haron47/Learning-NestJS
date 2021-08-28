ec7446e41ca47a0f55d143e425e19c4ce1c5c8e0
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-08-28 09:09:43 +0900

    typeorm seeding 오류 수정중

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,39 +11,12 @@ import { ChannelsService } from './channels/channels.service';
 import { WorkspacesService } from './workspaces/workspaces.service';
 import { WorkspacesController } from './workspaces/workspaces.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ChannelChats } from './entities/ChannelChats';
-import { ChannelMembers } from './entities/ChannelMembers';
-import { Channels } from './entities/Channels';
-import { DMs } from './entities/DMs';
-import { Mentions } from './entities/Mentions';
-import { Users } from './entities/Users';
-import { WorkspaceMembers } from './entities/WorkspaceMembers';
-import { Workspaces } from './entities/Workspaces';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'sleact',
-      entities: [
-        ChannelChats,
-        ChannelMembers,
-        Channels,
-        DMs,
-        Mentions,
-        Users,
-        WorkspaceMembers,
-        Workspaces,
-      ],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [
     AppController,
