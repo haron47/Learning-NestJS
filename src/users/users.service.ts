29b671e289410f4d80f1ffc0991f2e83ef90228c
angelkim88 <66551410+haron47@users.noreply.github.com>
2021-08-30 23:39:00 +0900

    request name

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
@@ -14,17 +19,17 @@ export class UsersService {
 
   async join(email: string, nickname: string, password: string) {
     if (!email) {
-      throw new HttpException('이메일 없네요', 400);
+      throw new BadRequestException('이메일 없네요');
     }
     if (!nickname) {
-      throw new HttpException('닉네임 없네요', 400);
+      throw new BadRequestException('닉네임 없네요');
     }
     if (!password) {
-      throw new HttpException('비밀번호 없네요', 400);
+      throw new BadRequestException('비밀번호 없네요');
     }
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
-      throw new HttpException('이미 존재하는 사용자입니다', 400);
+      throw new UnauthorizedException('이미 존재하는 사용자입니다');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
     await this.usersRepository.save({
