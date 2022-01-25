e83e3b3534c053b3b53cee82a0c3889381537d07
haron47 <pooh0407@ewhain.net>
2022-01-26 01:42:55 +0900

    feat: ttl option 추가

--- nest-redis/src/app.module.ts
+++ nest-redis/src/app.module.ts
@@ -5,7 +5,12 @@ import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
   imports: [
-    CacheModule.register({ store: redisStore, host: 'localhost', port: 6379 }),
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 100000,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
