db8eb5e103756c82a1834ed7100c8f3d57ab278e
haron47 <pooh0407@ewhain.net>
2022-01-26 01:24:39 +0900

    feat: redis test 라우터 생성

--- nest-redis/src/app.module.ts
+++ nest-redis/src/app.module.ts
@@ -1,19 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { Module, CacheModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: 'MATH_SERVICE',
-        transport: Transport.REDIS,
-        options: {
-          url: 'redis://localhost:6379',
-        },
-      },
-    ]),
+    CacheModule.register({ store: redisStore, host: 'localhost', port: 6379 }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- nest-redis/src/app.service.ts
+++ nest-redis/src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getCache() {
+    const savedTime = await this.cacheManager.get<number>('time');
+    if (savedTime) {
+      return 'saved time : ' + savedTime;
+    }
+    const now = new Date().getTime();
+    await this.cacheManager.set<number>('time', now);
+    return 'save new time : ' + now;
   }
 }
