c1aaced1c7150962d2910332af90d048aadf6473
haron47 <pooh0407@ewhain.net>
2022-02-02 01:57:42 +0900

    fix: add sentry capture exception

--- nest-sentry/src/common/webhook.interceptor.ts
+++ nest-sentry/src/common/webhook.interceptor.ts
@@ -7,18 +7,20 @@ import {
 import { captureException } from '@sentry/minimal';
 import { catchError, Observable, of } from 'rxjs';
 import { IncomingWebhook } from '@slack/client';
+import * as Sentry from '@sentry/minimal';
 
 @Injectable()
 export class WebhookInterceptor implements NestInterceptor {
   intercept(_: ExecutionContext, next: CallHandler) /** : Observable<any>*/ {
     return next.handle().pipe(
       catchError((error) => {
+        Sentry.captureException(error);
         const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
         webhook.send({
           attachments: [
             {
               color: 'danger',
-              text: '회사 드가자~ 드가자~!',
+              text: '에러가 발생했다!',
               fields: [
                 {
                   title: `Request Message: ${error.message}`,
