a1a3a51b6ee6827c7dd3200bbc4f324724bd7d38
hocaron <pooh0407@ewhain.net>
2022-04-18 19:04:34 +0900

    feat : queue에 task를 추가하기 위한 컨트롤러 및 서비스 생성

--- nest-message-queue/src/app.service.ts
+++ nest-message-queue/src/app.service.ts
@@ -1,7 +1,18 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectQueue('messageQueue') private messageQueue: Queue) {}
+
+  async addMessageQueue(data: number) {
+    const job = await this.messageQueue.add('task', {
+      dataId: data,
+    });
+    return job;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
