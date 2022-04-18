ec2b42bf9532344c0aac38ae963f05e4a1512bdb
hocaron <pooh0407@ewhain.net>
2022-04-18 19:05:03 +0900

    feat : 생성한 소비자를 app.module provider에 등록

--- nest-message-queue/src/app.module.ts
+++ nest-message-queue/src/app.module.ts
@@ -1,10 +1,20 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConsumer } from './app.consumer';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.registerQueue({
+      name: 'messageQueue',
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppConsumer],
 })
 export class AppModule {}
