1367ff8461f8076ccd7d1dcd8251b0f3957ec56c
hocaron <pooh0407@ewhain.net>
2022-04-22 03:45:17 +0900

    feat : message subscriber 생성

---
+++ nest-redis-pub-sub/src/message/message.subscriber.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { createClient } from 'redis';
+
+@Injectable()
+export class MessageSubscriber implements OnApplicationBootstrap {
+  onApplicationBootstrap() {
+    // 메일 삭제 푸시 알림 작동
+    this.subscribeMessage();
+  }
+
+  async subscribeMessage() {
+    const subscriber = createClient({ host: 'localhost', port: 6379 });
+    subscriber.subscribe('hocaron_channel');
+    subscriber.on('message', (channel, message) => {
+      console.log(message);
+    });
+    return;
+  }
+}
