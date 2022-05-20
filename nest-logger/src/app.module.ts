3196e212c47edcbf9a7d361592c37c4cd5b06c84
hocaron <pooh0407@ewhain.net>
2022-05-21 04:22:29 +0900

    feat : 로거 적용

--- nest-logger/src/app.module.ts
+++ nest-logger/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as winston from 'winston';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
 
 @Module({
-  imports: [],
+  imports: [
+        WinstonModule.forRoot({
+          transports: [
+            new winston.transports.Console({
+              level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+              format: winston.format.combine(
+                  winston.format.timestamp(),
+                  nestWinstonModuleUtilities.format.nestLike('MyApp', { prettyPrint: true }),
+              ),
+            }),
+          ],
+        }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-logger/src/app.service.ts
+++ nest-logger/src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Logger as WinstonLogger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: WinstonLogger,
+  ) { }
+
+  async createLog(): Promise<void> {
+    console.log(this.logger.name);
+
+    let data  = "data is";
+
+    this.logger.error('error: ', "hi");
+    this.logger.warn('warn: ', data);
+    this.logger.info('info: ', data);
+    this.logger.http('http: ', data);
+    this.logger.verbose('verbose: ', data);
+    this.logger.debug('debug: ', data);
+    this.logger.silly('silly: ', data);
   }
 }
